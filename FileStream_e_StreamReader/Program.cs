b86527eef6c7b03582cfd17b3f235438cfbde79c
Marlon Salles <marlonsalles@hotmail.com>
2022-05-01 15:00:26 -0300

    Criação de um objeto do tipo StreamReader e FileStream

---
+++ FileStream_e_StreamReader/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Ex 001 - Um pouco desconf. 
+            string path = @"C:\Users\marlo\AppData\Local\Temp\arquivo.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                fs = new FileStream(path, FileMode.Open); // File.OpenRead(path);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+                string a = Console.ReadLine();
+            }
+            */
+
+            string path = @"c:\temp\file1.txt";
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
